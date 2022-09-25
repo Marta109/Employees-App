3ff0abf6b1477a5dcf5578f0f7af0e5a63ea65ec
Marta109 <marta.hayrapetyan.a@gmail.com>
2022-09-25 18:06:22 +0400

    first commit

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,18 @@
+import "./app-filter.css";
+const AppFilter = () => {
+  return (
+    <div className="btn-group">
+      <button className="btn  btn-light" type="button">
+        All employees
+      </button>
+      <button className="btn btn-outline-light" type="button">
+        To enhance
+      </button>
+      <button className="btn btn-outline-light" type="button">
+        salary of more than 1000$
+      </button>
+    </div>
+  );
+};
+
+export default AppFilter;
